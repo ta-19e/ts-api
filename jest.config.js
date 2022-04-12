4cc5fac8e84948b37dc6124a482af19b7cde56ac
Siim Tiilen <siim.tiilen@tptlive.ee>
2022-04-12 13:36:28 +0300

    fix eslint for windows

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,13 @@ module.exports = {
   preset: 'ts-jest',
   globals: {
     'ts-jest': {
-      tsConfig: 'tsconfig.test.json'
-    }
+      tsConfig: 'tsconfig.test.json',
+    },
   },
   testMatch: ['**/(test|src)/**/*.(spec|e2e|test).(ts|js)'],
   testEnvironment: 'node',
   verbose: true,
   collectCoverage: true,
   collectCoverageFrom: ['./src/**/*.ts'],
-  coverageReporters: ['text', 'text-summary', 'lcov']
+  coverageReporters: ['text', 'text-summary', 'lcov'],
 };
